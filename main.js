c1dd7559fd286328816df02d33387f57a81c43c2
Paweł Słowik <ffpawel.ps2002@gmail.com>
2020-12-29 01:30:42 +0100

    dziala ale zle

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const play = document.querySelector("#play")
 
-
 var playing = true;
+
+var songs = ["Koza-Beton.mp3" , "hot16 Piorun ft. Dario.mp3" , "ZetHa ft. Borixon, ReTo - Wesoły Numer O Smutnych Rzeczach.mp3"], songIndex = 0;
+
 
 function playPause() {
     if (playing) {
@@ -15,4 +17,21 @@ function playPause() {
         playing = true;
         play.src = "./zdjęcia/play.png"
 }
+}
+
+
+
+
+function nextSong() {
+    songIndex++;
+    if (songIndex > 2) songIndex = 0;
+    document.querySelector("#song").src = songs[songIndex];
+    playPause()
+}
+
+function previousSong() {
+    songIndex--;
+    if (songIndex < 0) songIndex = 2;
+    document.querySelector("#song").src = songs[songIndex];
+    playPause()
 }
