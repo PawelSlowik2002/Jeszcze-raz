6679b7d7dc0adadad8c6b225ae72e5dcbed43f19
PawelSlowik2002 <ffpawel.ps2002@gmail.com>
2021-01-12 23:37:03 +0100

    byle sie nie zepsulo

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const play = document.querySelector("#play")
 const progressBar = document.querySelector("#progressBar")
+const songArtist = document.querySelector(".song__artist")
+const songTitle = document.querySelector(".song__title")
+
 var playing = true;
+
 
 var songs = ["Koza-Beton.mp3" , "hot16 Piorun ft. Dario.mp3" , "ZetHa ft. Borixon, ReTo - Wesoły Numer O Smutnych Rzeczach.mp3"], songIndex = 0;
 thumbnails = ["./zdjęcia/maxresdefault.jpg" , "./zdjęcia/hot16piorunmario.png", "./zdjęcia/wesolynumerosmutnychrzeczach.png"];
@@ -26,7 +30,9 @@ function nextSong() {
     songIndex++;
     if (songIndex > 2) songIndex = 0;
     document.querySelector("#song").src = songs[songIndex];
-    document.querySelector("#thumbnails").src = thumbnails[songIndex];
+    document.querySelector("#thumbnail").src = thumbnails[songIndex];
+    songArtist.innerHTML = songArtists[songIndex];
+    songTitle.innerHTML = songTitles[songIndex];
     playing = true;
     playPause()
 }
@@ -35,14 +41,10 @@ function previousSong() {
     songIndex--;
     if (songIndex < 0) songIndex = 2;
     document.querySelector("#song").src = songs[songIndex];
+    document.querySelector("#thumbnail").src = thumbnails[songIndex];
+    songArtist.innerHTML = songArtists[songIndex];
+    songTitle.innerHTML = songTitles[songIndex];
     playing = true;
     playPause()
 }
 
-function changeProgressValue() {
-    const progressBar = document.querySelector("#progressBar");
-    progressBar.max = document.querySelector("#song").duration;
-    progressBar.value = document.querySelector("song").currentTime;
-    changeProgressValue();
-}
-setInterval(changeProgressValue(), 700);
